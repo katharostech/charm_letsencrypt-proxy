b790a01b1994203d75fb2cfea1a149b673cf285e
Zicklag <zicklag@katharostech.com>
2020-02-01 23:49:11 +0000

    Hone the HAProxy Config
    Get the haproxy config working and a default certificate installed.

--- docker/docker-cmd.sh
+++ docker/docker-cmd.sh
@@ -5,11 +5,14 @@ certs_dir=$config_dir/certs
 config_template=$config_dir/haproxy.cfg.gomplate
 config_file=$config_dir/haproxy.cfg
 
-# Make sure certs dir exists
-mkdir -p $certs_dir
-
 # Write template data to yaml file
 echo "$HAPROXY_CFG_TPL_DATA" > data.yaml
 
 # Expand config template to haproxy config file
 gomplate -d data.yaml -f $config_template > $config_file
+
+# Start cron
+crond
+
+# Start HAProxy
+haproxy -- $config_file
