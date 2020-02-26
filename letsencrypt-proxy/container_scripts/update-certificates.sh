b0e1e921cb30e23f06ffe0fc6cedae8dc0f3ca60
Zicklag <zicklag@katharostech.com>
2020-02-26 22:04:26 +0000

    Attempt to Fix update-certificates.sh Update
    The `update-certificiates.sh` script was not logging the acme.sh cron job
    in append mode, and it was also failing to run the `lucky leader set` for some
    reason. Hopefully this fixes it.

--- letsencrypt-proxy/container_scripts/update-certificates.sh
+++ letsencrypt-proxy/container_scripts/update-certificates.sh
@@ -10,12 +10,13 @@ if [ "$(lucky leader is-leader)" = "true" ]; then
     lucky set-status maintenance "Updating certificates"
 
     # Renew certificates if necessary
-    acme.sh --cron --config-home $ACME_CFG_HOME > /var/log/acmesh.log 2>&1
+    echo "Running cert update cron job from update-certificates.sh charm script" >> /var/log/acmesh.log
+    acme.sh --cron --config-home $ACME_CFG_HOME >> /var/log/acmesh.log 2>&1
 
     # Get the previous acme config dir data
-    previous_acme_cfg_base64=$(lucky leader get acme_cfg_base64 | tr -d '\n')
+    previous_acme_cfg_base64="$(lucky leader get acme_cfg_base64 | tr -d '\n')"
     # Get the latest config
-    acme_cfg_base64=$(tar -cz $ACME_CFG_HOME | base64 | tr -d '\n')
+    acme_cfg_base64="$(tar -cz $ACME_CFG_HOME | base64 | tr -d '\n')"
 
     # If the latest config is different than the previous config
     if [ "$previous_acme_cfg_base64" != "$acme_cfg_base64" ]; then
